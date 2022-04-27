914be829101706f0a94af53b6b0d7274a5913787
NataliaLP <96068088+NataliaLP@users.noreply.github.com>
2022-04-27 19:45:20 +0200

    ajout du choix de la couleur

--- front/JS/product.js
+++ front/JS/product.js
@@ -9,18 +9,32 @@ fetch(`http://localhost:3000/api/products/${sofaId}`)
     .then((response) => {
         return response.json();
     })
-    .then((product) => {
-        displaySofa(product);
+    .then((sofa) => {
+        displaySofa(sofa);
     })
     .catch((error) => {
         alert(error);
     });
     
 
-let displaySofa = (product) => {
+let displaySofa = (sofa) => {
     document.querySelector(".item__img").innerHTML +=
-    `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
-    document.getElementById("title").textContent = product.name;
-    document.getElementById("price").textContent = product.price;
-    document.getElementById("description").textContent = product.description;
+    `<img src="${sofa.imageUrl}" alt="${sofa.altTxt}">`;
+    document.getElementById("title").textContent = sofa.name;
+    document.getElementById("price").textContent = sofa.price;
+    document.getElementById("description").textContent = sofa.description;
+
+     // selection de la couleur dans le html
+     let ColorId = document.querySelector("#colors");
+    
+     //itération dans le tableau color de l'objet et insertion des variables dans le html 
+    for (color of sofa.colors){
+        let optionColors = document.createElement("option");
+        optionColors.innerHTML =`${color}`;
+        optionColors.value =`${color}`;
+        ColorId.appendChild(optionColors);
+    }
 }
+
+
+
