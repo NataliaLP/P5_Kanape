949c23f6a031a831ff5f1ad8f9baa9552f7f3c69
NataliaLP <96068088+NataliaLP@users.noreply.github.com>
2022-05-05 14:16:03 +0200

    enregistrer la selection faite par l'utilisateur

--- front/JS/product.js
+++ front/JS/product.js
@@ -18,6 +18,7 @@ fetch(`http://localhost:3000/api/products/${sofaId}`)
     
 
 let displaySofa = (sofa) => {
+    // recuperation des caracteristiques du produit
     document.querySelector(".item__img").innerHTML +=
     `<img src="${sofa.imageUrl}" alt="${sofa.altTxt}">`;
     document.getElementById("title").textContent = sofa.name;
@@ -30,11 +31,61 @@ let displaySofa = (sofa) => {
      //itération dans le tableau color de l'objet et insertion des variables dans le html 
     for (color of sofa.colors){
         let optionColors = document.createElement("option");
-        optionColors.innerHTML =`${color}`;
-        optionColors.value =`${color}`;
+        optionColors.innerHTML = color;
+        optionColors.value = color;
         ColorId.appendChild(optionColors);
     }
 }
+
+
+//------Tableau qui contiendra les articles que l'utilisateur souhaite mettre dans le panier---
+const cartArray = {
+    'sofa' : []
+}
+
+
+const btnAddCard = document.getElementById("addToCart")
+let colorSelect = document.getElementById("colors").value;
+let quantitySelect = document.getElementById("quantity").value;
+
+
+let sofaSelect = (sofa) => {
+    btnAddCard.addEventListener("click", (event) => {
+    if (document.getElementById("colors").value == undefined) {
+        confirm("Veuillez sélectionner une couleur ");
+    } 
+    
+    
+    else if (document.getElementById("quantity").value == 0) {
+        confirm("Veuillez sélectionner le nombre d'article souhaité");  
+    } 
+    
+    
+    else if (document.getElementById("quantity").value > 100) {
+        confirm("Les commandes sont limitées à 100 articles par références"); 
+    } 
+    
+    
+    else {
+      alert("Cet article a été ajouté à votre panier."); 
+      let sofaSelectToCart = {
+        id: getSofaId,
+        title: sofa.name,
+        quantity: quantitySelect,
+        color: colorSelect,
+        img: sofa.imageUrl,
+        alt: sofa.altTxt,
+        description: sofa.description,
+        price: sofa.price * quantitySelect,
+      };
+
+
+    }
+    })}
+
+    console.log(sofaSelect)
+
+  
 
 
 
